6e4fef97e3b54b0277abeb2a7d077316e78eb4a1
Amos <note@amosl.com>
2021-10-27 10:58:22 -0400

    Add new component

---
+++ Source/CoopGame/Private/Components/SHealthComponent.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SHealthComponent.h"
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+	m_Health = 100;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+
